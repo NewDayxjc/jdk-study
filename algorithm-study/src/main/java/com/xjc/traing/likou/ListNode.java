ee2625217bdbb4d18ebda6e5e30041aa4327022a
xiongjc <2323948060@qq.com>
2020-10-23 18:21:02 +0800

    回文

---
+++ algorithm-study/src/main/java/com/xjc/traing/likou/ListNode.java
@@ -0,0 +1,28 @@
+package com.xjc.traing.likou;
+
+/**
+ * @author XiongJinChen
+ * @version 1.0
+ * @date 2020/10/20 11:12
+ * @name ListNode
+ * 请判断一个链表是否为回文链表。
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(Integer.MAX_VALUE);
+
+    }
+
+    public boolean isPalindrome(ListNode node) {
+
+        return false;
+    }
+
+}
